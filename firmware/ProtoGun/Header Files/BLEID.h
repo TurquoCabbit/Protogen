263e50928472cf6638be23151dcfd08ae52ee43f
TurquoCabbit <night5916lan@gmail.com>
2020-12-15 19:56:57 +0800

    add Face2header output to git

---
+++ firmware/ProtoGun/Header Files/BLEID.h
@@ -0,0 +1,13 @@
+//2020/12/15 19:11:47		00_Spoon
+
+#define ProtoGen_ID	00
+#if 0
+#elif (ProtoGen_ID == 00)
+	#define SERVICE_UUID "5008e8ff-8b1d-44eb-bdac-7b574fb7607e"
+	#define CHARACTERISTIC_UUID "c854f213-6577-498a-8b84-db4e014427bc"
+	#define ProtoGun_Name "ProtoBlaster_00"
+#elif (ProtoGen_ID == 01)
+	#define SERVICE_UUID "49950fa5-3cc0-4c1f-86a8-6f86a8977b91"
+	#define CHARACTERISTIC_UUID "0e26662f-762c-4c04-994a-22fd392330c0"
+	#define ProtoGun_Name "ProtoBlaster_01"
+#endif
