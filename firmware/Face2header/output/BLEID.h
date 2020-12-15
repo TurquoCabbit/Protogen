84efd9257d41439cc2c47207ed70bb17542160c6
TurquoCabbit <night5916lan@gmail.com>
2020-12-15 17:06:08 +0800

    modify .gitignore file

---
+++ firmware/Face2header/output/BLEID.h
@@ -0,0 +1,13 @@
+//2020/12/15 17:01:24		00_Spoon
+
+#define ProtoGen_ID	00
+#if 0
+#elif (ProtoGen_ID == 00)
+	#define SERVICE_UUID "70311a6d-be67-4885-8e14-16ef71116b04"
+	#define CHARACTERISTIC_UUID "41926ae3-8840-4e3f-a84a-ee9fd45e9993"
+	#define ProtoGun_Name "ProtoBlaster_00"
+#elif (ProtoGen_ID == 01)
+	#define SERVICE_UUID "cb251895-5de4-4d92-8743-e6e374df3200"
+	#define CHARACTERISTIC_UUID "16b17058-d34b-4e8b-8120-3ba39de3054a"
+	#define ProtoGun_Name "ProtoBlaster_01"
+#endif
