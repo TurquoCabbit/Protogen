75c590f27f64bf92090a50e2818cd166a490a398
TurquoCabbit <night5916lan@gmail.com>
2021-01-24 13:17:08 +0800

    2021-01-24 modify protogen_matrix GPIO pin mapping

--- firmware/Protogen_matrixs/Header Files/GPIO.h
+++ firmware/Protogen_matrixs/Header Files/GPIO.h
@@ -1,9 +1,9 @@
 //Pin define
-#define LED_PIN			15   //Neopixel
-#define FAN_PIN			16   //Fan
-#define BZ_PIN			18   //bz
-#define IR_Rx_PIN		5
-#define IR_Tx_PIN		17
+#define LED_PIN			15	//Neopixel
+#define FAN_PIN			27	//Fan
+#define BZ_PIN			26	//bz
+#define IR_Tx_PIN		25	//boop sence tx
+#define IR_Rx_PIN		35	//boop sence rx
 
 #define BZ_channel	0
 #define IR_channel	2
@@ -21,7 +21,7 @@ inline void gpio_init(void)
 	pinMode(BZ_PIN, OUTPUT);
 	digitalWrite(BZ_PIN, 0);
 
-	pinMode(IR_Rx_PIN, INPUT_PULLUP);
+	pinMode(IR_Rx_PIN, INPUT);
 	
 	pinMode(IR_Tx_PIN, OUTPUT);
 	ledcAttachPin(IR_Tx_PIN, IR_channel);
